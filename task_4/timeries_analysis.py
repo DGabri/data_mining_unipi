9f4a52186c96acb652996ff5b3171086877ce88b
DGabri <gabriele@ntop.org>
2025-12-13 15:20:19 +0100

    Added mp3 timeseries extraction and initial data validation

--- task_4/timeries_analysis.py
+++ task_4/timeries_analysis.py
@@ -5,64 +5,20 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 import librosa
+import time
 import os
 
 # base path
 mp3_base_path = "../fedez_fibra/"
-tracks_list = os.listdir(mp3_base_path)
+dataset_output_dir = "../timeseries_datasets/"
 
-number_tracks = len(tracks_list)
-print(f"Loaded: {number_tracks} mp3")
+df = pd.read_csv("../timeseries_datasets/tracks_timeseries.csv")
 
-# check if tracks are in the origianl tracks dataset
-tracks = pd.read_csv("../original_datasets/tracks.csv")
+df['centroid_num_samples'] = df["centroid"].apply(lambda x: len(x))
+df['rollof_num_samples'] = df["rolloff"].apply(lambda x: len(x))
+df['flux_num_samples'] = df["flux"].apply(lambda x: len(x))
+df['rms_num_samples'] = df["rms"].apply(lambda x: len(x))
+df['zcr_num_samples'] = df["zcr"].apply(lambda x: len(x))
+df['spectral_bw_num_samples'] = df["spectral_bw"].apply(lambda x: len(x))
 
-# parse fname -> artistID - trackID
-tracks_info = []
-
-for track in tracks_list:
-    # :-4 take fname without .mp3
-    # split on " - " to get left and right part
-    splitted_fname = track[:-4].split(" - ")
-    artist_id = splitted_fname[0]
-    track_id = splitted_fname[1]
-    
-    track_info = {"id": track_id, "id_artist": artist_id}
-    tracks_info.append(track_info)
-
-tracks_info_df = pd.DataFrame(tracks_info)
-
-# extract artists id and correlate it to name
-unique_artists = tracks_info_df.id_artist.unique()
-artists_names = list(tracks[tracks['id_artist'].isin(unique_artists)]['name_artist'].unique())
-
-# check how many tracks are in the tracks df
-track_matches = tracks_info_df['id'].isin(tracks['id']).sum()
-missing_tracks_count = number_tracks - track_matches
-
-print(f"Total mp3 tracks: {number_tracks} Matches found: {track_matches} Missing tracks: {missing_tracks_count}")
-
-print(f"Artists names: {', '.join(artists_names)}")
-
-#########################################################
-# data exploration
-# load all files, extract sampling rate and shape of data
-
-tracks_stats = []
-
-for i in range(number_tracks):
-    fname = tracks_list[i]
-    data, sr = librosa.load(mp3_base_path+fname)
-    num_samples = data.shape[0]
-    track_info = {"id": fname, "num_samples": num_samples, "sr": sr}
-    tracks_stats.append(track_info)
-    print(f"[{i}/{number_tracks}]Loaded: {fname}")
-
-
-# test plot one timeseries
-data, sr = librosa.load(mp3_base_path+tracks_list[2])
-
-df = pd.DataFrame(data, columns=["value"])
-
-fig = px.line(df, x=df.index, y=df['value'], title='data samples')
-fig.show()
+df
